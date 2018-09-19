7a2ed2d26b146728e2935bb3a73b2f8bf982701a
James DeLayo <TuckingFypos@users.noreply.github.com>
2018-09-19 13:20:20 -0400

    Adds Open-All functionality.
    Incorporates a JQuery button at the bottom of the Output File that allows the user to open all gathered links simultaneously in separate tabs.

--- URLExpander.py
+++ URLExpander.py
@@ -7,19 +7,37 @@ file_name = name+".html"
 origin_string = str(origin)
 
 with open(file_name, 'a') as out:
-    out.write('<!DOCTYPE html><html><head><meta charset="UTF-8"><title>title</title></head><body>')
+    out.write('<!DOCTYPE html><html><head><meta charset="UTF-8"><title>title</title></head>')
     out.write('\n')
+    out.write('<script src="https://code.jquery.com/jquery-latest.js"></script>')
+    out.write('\n')
+    out.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>')
+    out.write('\n')
+    out.write('<body>')
 
 for x in range(int(range_bot), int(range_top)+1):
     new_list = origin_string.replace("#", str(x))
     print(new_list)
     with open(file_name, 'a') as out:
         out.write('<a href="'+new_list+'">'+new_list+'</a>')
+        out.write('\n')
     with open(file_name, 'a') as out:
         out.write('<br>')
+        out.write('\n')
     with open(file_name, 'a') as out:
         out.write('<br>')
+        out.write('\n')
 
 with open(file_name, 'a') as out:
-    out.write('</body></html>')
+    out.write('<button id="openButton">Open</button>')
+    out.write('\n')
+    out.write('</body>')
+    out.write('\n')
+
+with open(file_name, 'a') as out:
+    out.write("<script>$('#openButton').click(function() {$('a').each(function() {window.open($(this).attr('href') );});});</script>")
+    out.write('\n')
+
+with open(file_name, 'a') as out:
+    out.write('</html>')
 
